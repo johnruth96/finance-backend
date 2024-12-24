f3b47406cdf3fa2699a5c5a02a6ccaed686f89ea
Johannes Ruthmann <j.ruthmann@online.de>
2024-12-24 01:13:04 +0100

    switch to console logging

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -36,22 +36,3 @@ DATABASES = {
         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
     },
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/app/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
